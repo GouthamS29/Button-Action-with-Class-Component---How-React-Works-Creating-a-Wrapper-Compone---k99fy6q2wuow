b3811ad8e01912d2ae626c9856d716dd7c41b120
GouthamS29 <sambarajugoutham1994@gmail.com>
2024-04-12 00:44:25 +0530

    assignment done

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,24 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state = {
+			changer : false,
+		};
 	};
+
+	displayMessage = () => {
+		this.setState({changer : true})
+	}
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				{this.state.changer === false ? null : (
+					<p id="para">
+						Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+					</p>
+				)}
+				<button id="click" onClick={this.displayMessage}>Click Me</button>
     		</div>
     	);
     }
